28c0b553dc00982f1aba8726afb0ebdca09a4954
yordanos yadessa <yordanosyadessa2019@gmail.com>
2023-08-14 16:31:28 +0300

    added scrollable cards in the welcome page

--- new_proj/lib/welcome.dart
+++ new_proj/lib/welcome.dart
@@ -11,7 +11,10 @@ class Welcome extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              const Text("Welcome, Yordi \u{1F44B} "),
+              const Text("Welcome, Yordi \u{1F44B} ",
+              style: TextStyle(
+                fontFamily: "WorkSans"
+              ),),
               const SizedBox(height: 10.0),
               Row(
                 children: [
@@ -20,7 +23,8 @@ class Welcome extends StatelessWidget {
                       "Let's find lunch near you ",
                       style: TextStyle(
                           fontSize: 20.0,
-                          fontWeight: FontWeight.w400,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: "WorkSans",
                           letterSpacing: 1.0),
                     ),
                   ),
@@ -64,7 +68,9 @@ class Welcome extends StatelessWidget {
                     prefixIcon: Icon(Icons.search),
                     labelText: 'Search',
                     labelStyle: TextStyle(
-                        fontWeight: FontWeight.w300
+                        fontWeight: FontWeight.w300,
+                        letterSpacing: 1,
+                        color: Colors.grey
                     )
                   ),
                 ),
@@ -83,12 +89,274 @@ class Welcome extends StatelessWidget {
                   SizedBox(width: 200),
                   Text("All",
                   style: TextStyle(
-                    fontWeight: FontWeight.w300,
                     fontSize: 18,
-                    fontFamily: "WorkSans"
+                    fontFamily: "WorkSans",
+                    color: Colors.grey
                   ),)
                 ],
-              )
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 240,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        width: 200,
+                        child: Card(
+                            elevation:4,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Column(
+                              children: [
+                                const ClipRRect(
+                                  borderRadius: BorderRadius.only(topLeft: Radius.circular(8), topRight: Radius.circular(8)),
+                                  child: Image(
+                                    fit: BoxFit.cover, 
+                                    image: AssetImage("assets/cover.jpg"),
+                                    ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      const Text(
+                                        'chicken Fillet',
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: "WorkSans"
+                                        ),
+                                      ),
+                                      Text(
+                                        'Description',
+                                        style: TextStyle(
+                                          color: Colors.grey[700],
+                                        ),
+                                      ),
+                                      Row(
+                                        children:  [
+                                          const Text("250 ETB"),
+                                          const SizedBox(width: 70),
+                                          Container(
+                                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(8),
+                                            color:const  Color.fromARGB(
+                                                  255, 237, 78, 10)),
+                                              
+                                              child: IconButton(
+                                                  onPressed: () {
+                                              // Add your button action here
+                                            },
+                                                  icon: Icon(Icons.shopping_cart),
+                                                  color: Colors.white,
+                                              ),
+                                            ),
+                                          
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                      ),
+                      Container(
+                        width: 200,
+                        child: Card(
+                          elevation: 4,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Column(
+                            children: [
+                              const ClipRRect(
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(8),
+                                    topRight: Radius.circular(8)),
+                                child: Image(
+                                  fit: BoxFit.cover,
+                                  image: AssetImage("assets/cover.jpg"),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      'chicken Fillet',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: "WorkSans"),
+                                    ),
+                                    Text(
+                                      'Description',
+                                      style: TextStyle(
+                                        color: Colors.grey[700],
+                                      ),
+                                    ),
+                                    Row(
+                                      children: [
+                                        const Text("250 ETB"),
+                                        const SizedBox(width: 70),
+                                        Container(
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(8),
+                                              color: const Color.fromARGB(
+                                                  255, 237, 78, 10)),
+                                          child: IconButton(
+                                            onPressed: () {
+                                              // Add your button action here
+                                            },
+                                            icon: Icon(Icons.shopping_cart),
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 200,
+                        child: Card(
+                          elevation: 4,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Column(
+                            children: [
+                              const ClipRRect(
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(8),
+                                    topRight: Radius.circular(8)),
+                                child: Image(
+                                  fit: BoxFit.cover,
+                                  image: AssetImage("assets/cover.jpg"),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      'chicken Fillet',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: "WorkSans"),
+                                    ),
+                                    Text(
+                                      'Description',
+                                      style: TextStyle(
+                                        color: Colors.grey[700],
+                                      ),
+                                    ),
+                                    Row(
+                                      children: [
+                                        const Text("250 ETB"),
+                                        const SizedBox(width: 70),
+                                        Container(
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(8),
+                                              color: const Color.fromARGB(
+                                                  255, 237, 78, 10)),
+                                          child: IconButton(
+                                            onPressed: () {
+                                              // Add your button action here
+                                            },
+                                            icon: Icon(Icons.shopping_cart),
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 200,
+                        child: Card(
+                          elevation: 4,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Column(
+                            children: [
+                              const ClipRRect(
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(8),
+                                    topRight: Radius.circular(8)),
+                                child: Image(
+                                  fit: BoxFit.cover,
+                                  image: AssetImage("assets/cover.jpg"),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      'chicken Fillet',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: "WorkSans"),
+                                    ),
+                                    Text(
+                                      'Description',
+                                      style: TextStyle(
+                                        color: Colors.grey[700],
+                                      ),
+                                    ),
+                                    Row(
+                                      children: [
+                                        const Text("250 ETB"),
+                                        const SizedBox(width: 70),
+                                        Container(
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(8),
+                                              color: const Color.fromARGB(
+                                                  255, 237, 78, 10)),
+                                          child: IconButton(
+                                            onPressed: () {
+                                              // Add your button action here
+                                            },
+                                            icon: Icon(Icons.shopping_cart),
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
